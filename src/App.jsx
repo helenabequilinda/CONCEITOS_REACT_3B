05a948a33a9e07fad52abc6069248c739ee80e33
HELENA PANGUSSAT BEQUI <helena_bequi@estudante.sesisenai.org.br>
2026-04-30 11:46:13 -0300

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import { useState } from 'react'
 import './App.css'
+import Cadastro from '../paginas/cadastro'
+import Login from '../paginas/login'
+import Usuario from '../paginas/usuario'
 
 function App() {
+  const [tela, setTela] = useState('login')
+
+  const renderizarTela = () => {
+    if(tela === 'login') {
+      return <Login/>
+    } else if (tela === 'cadastro'){
+      return <Cadastro/>
+    } else if (tela === 'usuario'){
+      return <Usuario/>
+    }
+  }
   
+  const trocarTela = (pagina) => {
+    setTela(pagina)
+  }
+
 
   return (
     <>
-      <section id="center">
-
-      </section>
+      <h1>pikachu</h1>
+      <button onClick={ () => trocarTela('login')}>login</button>
+      <button onClick={ () => trocarTela('cadastro')}>cadastro</button>
+      <button onClick={ () => trocarTela('usuario')}>usuario</button>
+      {renderizarTela()}
+      
     </>
   )
 }
