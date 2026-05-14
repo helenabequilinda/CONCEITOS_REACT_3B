e4fa4b18a7dafa8b879ff6c543c5e7dfd1804e3e
HELENA PANGUSSAT BEQUI <helena_bequi@estudante.sesisenai.org.br>
2026-05-14 11:55:04 -0300

    react

--- paginas/usuario.jsx
+++ paginas/usuario.jsx
@@ -2,19 +2,39 @@ import { useEffect, useState } from "react"
 
 export default function Usuario () {
     const [contador, setContador] = useState(0)
+    const [usuarios, setUsuarios] = useState([])
 
     useEffect(() => {
-        document.title = 'contagem ' + contador
+        document.title = 'contagem ' + contador;
+        const buscarUsuarios = async () => {
+            const resposta = await fetch('http://localhost:3000/usuarios')
+            const data = await resposta.json()
+            setUsuarios(data);
+
+            console.log(data);
+        }
+        buscarUsuarios()
+
     }, [contador])
 
 
     return (
-        <div>
+    <div>
             <h1>Usuario</h1>
             <h1>{contador}</h1>
 
             <button onClick= {() =>{setContador(contador + 1)}}>somar</button>
-        </div>
+        <ul>
+            {usuarios.map( (usuario) => (
+                <li key={usuario.id}>
+                    <b>{usuario.nome}</b>
+                    STATUS: { usuario.ativo ? 'Ativo' : 'Desativo' }
+                </li>
+            ))}
+
+        </ul>
+
+    </div>
        
     )
 }
